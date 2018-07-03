1dd8453c46abc614d4262d61620c737df4ea757d
fernandopaniagua <fernando.paniagua@gmail.com>
2018-07-03 21:05:40 +0200

    Fin Dia 4 Practicas
    Los malos aparecen desde el generador, se pone la barra de vida y el punto de mira. Aparece la animación de la muerte de los malos, pero aparece muy abajo.

--- Assets/_GameObjects/Scripts/EnemigoTontoScripts.cs
+++ Assets/_GameObjects/Scripts/EnemigoTontoScripts.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class EnemigoTontoScripts : MonoBehaviour {
     [SerializeField] float velocidadMovimiento = 5;
     CharacterController miCC;
+    [SerializeField] GameObject prefabParticulasMuerte;
 
     private void Start()
     {
-        InvokeRepeating("CambiarDireccion", 0, Random.Range(3, 5));
+        InvokeRepeating("CambiarDireccion", Random.Range(3,5),Random.Range(3, 5));
     }
 
     private void Awake()
@@ -25,6 +26,10 @@ public class EnemigoTontoScripts : MonoBehaviour {
         Debug.Log("¡AY!");
         Vector3 rotacionAAplicar = new Vector3(0, 180, 0);
         this.transform.Rotate(rotacionAAplicar);
+        GameObject nuevasParticulasMuerte = Instantiate(prefabParticulasMuerte);
+        nuevasParticulasMuerte.transform.position = this.transform.position;
+        nuevasParticulasMuerte.transform.up += Vector3.up * 3;
+        Destroy(this.gameObject);
     }
 
     private void CambiarDireccion()
---
+++ Assets/_GameObjects/Scripts/GeneradorEnemigos.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneradorEnemigos : MonoBehaviour {
+    [SerializeField] GameObject prefabEnemigo;
+    [SerializeField] int numeroEnemigosAGenerar=10;
+    [SerializeField] float tiempoEntreEnemigos=4;
+    [SerializeField] float radioZonaGeneracion=10;
+    [SerializeField] float alturaInvocacion = 1;
+
+    int numeroEnemigosGenerado = 0;
+
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("GenerarEnemigo", 0, tiempoEntreEnemigos);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void GenerarEnemigo()
+    {
+        if (numeroEnemigosGenerado < numeroEnemigosAGenerar)
+        {
+            Vector3 desplazamientoAleatorio =
+            Random.insideUnitSphere * radioZonaGeneracion;
+            desplazamientoAleatorio.y = 0;
+
+            GameObject nuevoEnemigo = Instantiate(prefabEnemigo);
+            nuevoEnemigo.transform.position =
+                this.transform.position + desplazamientoAleatorio;
+            nuevoEnemigo.transform.position +=
+                Vector3.up * alturaInvocacion;
+            numeroEnemigosGenerado++;
+        } else
+        {
+            CancelInvoke("GenerarEnemigo");
+        }
+        
+    }
+}
