c31f14dc59c896a4abf12f26b46920eca365bb62
fernandopaniagua <fernando.paniagua@gmail.com>
2018-07-19 22:01:44 +0200

    FIN SESION 2/7 UI_01

---
+++ Assets/_GameAssets/Scripts/UI/MenuPrincipal.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPrincipal : MonoBehaviour {
+
+	public void EmpezarJuego()
+    {
+        SceneManager.LoadScene(1);//Congela
+        //SceneManager.LoadScene("1", LoadSceneMode.Additive);//Or single Additive superpone las escenas
+        //SceneManager.LoadSceneAsync("1");//Asíncrona
+    }
+    public void SalirJuego()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/_GameAssets/Scripts/UI/NubesPortadaScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NubesPortadaScript : MonoBehaviour {
+    [SerializeField] float speed = 2;//Velocidad de movimiento de la nube
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector3.left * Time.deltaTime * speed);
+		
+	}
+}
